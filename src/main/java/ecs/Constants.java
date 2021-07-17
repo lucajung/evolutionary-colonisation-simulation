78d6dcd353d3eee5c239252cfbaa4c2d1bbd53c6
Luca Jung <luca@luca-jung.de>
2021-07-17 17:57:18 +0200

    change constants

--- src/main/java/ecs/Constants.java
+++ src/main/java/ecs/Constants.java
@@ -1,8 +1,8 @@
 package ecs;
 
 public class Constants {
-    public static final int MAP_X_TILES = 100;
-    public static final int MAP_Y_TILES = 100;
+    public static final int MAP_X_TILES = 75;
+    public static final int MAP_Y_TILES = 75;
     public static final int BACKGROUND_MAP_IMAGE_WIDTH = 800;
     public static final int BACKGROUND_MAP_IMAGE_HEIGHT = 800;
     public static final double MAP_CREATION_FACTOR = 0.03;
